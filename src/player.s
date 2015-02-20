7150d1c4544eca675b03a434b24aa73cab032159
saniv <snv985@gmail.com>
2015-02-20 21:42:11 +0400

    pentagram sprite

--- src/player.s
+++ src/player.s
@@ -3,7 +3,7 @@ type ai{player} world
 
 ai.update = 0
 
-type player{id world} name ai human color power moves
+type player{id world} name ai human color power moves spells
 | $ai <= ai Me
 
 // player 0 shouldnt be used
