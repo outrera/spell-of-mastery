11bc720fc0a383503b1657389fdb4ae828b38cfd
saniv <snv985@gmail.com>
2015-08-16 02:35:43 +0300

    improved hit frames

--- src/sprite.s
+++ src/sprite.s
@@ -26,7 +26,7 @@ type sprite{Bank Name height/1 xy/[0 0]
   | case Frames [[`-` time N]@Fs]: Frames <= Fs{[? N]}
   | [Name Frames]
 | when got $anims.death and no $anims.hit:
-  | $anims.hit <= [[$anims.death.0.0 24]] //supply default hit anim
+  | $anims.hit <= [[$anims.idle.0.0 24]] //supply default hit anim
 
 sprite.anim_speed AnimName =
 | Anim = $anims.AnimName
