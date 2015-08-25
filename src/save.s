3989b3a8425bb9f97422b37cb9d46740f13be60b
saniv <snv985@gmail.com>
2015-08-25 14:58:06 +0300

    fixed loading map with missing acts

--- src/save.s
+++ src/save.s
@@ -95,7 +95,8 @@ world.load Saved =
     | for I PS.size PS.I.init{Sight.I^rle_decode}
 | AEs = Saved.actions_enabled
 | Acts = $main.params.acts
-| when got AEs: for Name,Enabled AEs: Acts.Name.enabled <= Enabled
+| when got AEs: for Name,Enabled AEs:
+  | when got Acts.Name: Acts.Name.enabled <= Enabled
 
 main.load Path =
 | File = Path.get
