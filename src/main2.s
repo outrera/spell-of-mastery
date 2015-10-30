106a825995c653c72269e7c63a5984c3a2048a1c
saniv <snv985@gmail.com>
2015-10-30 23:19:54 +0300

    update

--- src/main2.s
+++ src/main2.s
@@ -43,7 +43,7 @@ type main{Data}
                "\n\n\nTHANKS TO:\n"
                "  Matthew Ostil\n"
               ]
-| sprite_dump Me "/Users/nikita/Downloads/1" unit_goblin
+| sprite_dump Me "/Users/nikita/Downloads/1" staticFrame/128 unit_knight
 | world Me
 
 export main
