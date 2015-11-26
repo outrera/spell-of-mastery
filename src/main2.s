fd32df633b8372746378e4d668a8e418760752c5
saniv <snv985@gmail.com>
2015-11-26 07:40:35 +0300

    knight/priest/wraith color correction

--- src/main2.s
+++ src/main2.s
@@ -43,7 +43,7 @@ type main{Data}
                "\n\n\nTHANKS TO:\n"
                "  Matthew Ostil\n"
               ]
-| sprite_dump Me "/Users/nikita/Downloads/1" staticFrame/100 sheet/1 unit_lizard
+//| sprite_dump Me "/Users/nikita/Downloads/1" staticFrame/128 sheet/0 unit_elf
 | init_unit_flags Me
 | world Me
 
