72fecf94a9fefb35beb154ca31534040ba479392
saniv <snv985@gmail.com>
2015-11-27 05:03:02 +0300

    restored older wizard sprite

--- src/main2.s
+++ src/main2.s
@@ -43,7 +43,7 @@ type main{Data}
                "\n\n\nTHANKS TO:\n"
                "  Matthew Ostil\n"
               ]
-//| sprite_dump Me "/Users/nikita/Downloads/1" staticFrame/128 sheet/0 unit_witch
+//| sprite_dump Me "/Users/nikita/Downloads/1" staticFrame/128 sheet/1 unit_wizard
 | init_unit_flags Me
 | world Me
 
