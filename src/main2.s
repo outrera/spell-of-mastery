1a3961661df816555ab6c3384a20dd6b5c385e88
saniv <snv985@gmail.com>
2016-01-04 14:30:31 +0300

    goblin recolor

--- src/main2.s
+++ src/main2.s
@@ -43,7 +43,7 @@ type main{Data}
                "\n\n\nTHANKS TO:\n"
                "  Matthew Ostil\n"
               ]
-//| sprite_dump Me "/Users/nikita/Downloads/1" staticFrame/210 sheet/1 unit_dragon
+| sprite_dump Me "/Users/nikita/Downloads/1" staticFrame/96 sheet/1 unit_goblin
 | init_unit_flags Me
 | world Me
 
