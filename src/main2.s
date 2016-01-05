802907bbb5740ba008282049978bb5ef1c1d1c84
saniv <snv985@gmail.com>
2016-01-05 17:06:43 +0300

    knight recolor

--- src/main2.s
+++ src/main2.s
@@ -43,7 +43,7 @@ type main{Data}
                "\n\n\nTHANKS TO:\n"
                "  Matthew Ostil\n"
               ]
-//| sprite_dump Me "/Users/nikita/Downloads/1" staticFrame/96 sheet/1 unit_elf
+//| sprite_dump Me "/Users/nikita/Downloads/1" staticFrame/96 sheet/1 unit_knight
 | init_unit_flags Me
 | world Me
 
