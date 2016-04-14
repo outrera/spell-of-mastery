686e221ee5e3914b2cf0c3ad7a1d97584e72e8a4
saniv <snv985@gmail.com>
2016-04-14 21:29:07 +0300

    more fixes to stairs tiling

--- src/tile.s
+++ src/tile.s
@@ -58,7 +58,7 @@ m_any_stairs Me X Y Z Role =
     $filled{X Y-1 Z} $filled{X+1 Y Z}
     $filled{X Y+1 Z} $filled{X-1 Y Z}
 //| D = E.digits{2}
-//| when [X Y]><[5 4]: say [Role [X Y Z] E]
+//| when [X Y]><[4 3]: say [Role [X Y Z] E]
 | if E><[1 0 0 1] then
    | if $role{X+1 Y Z-4}><Role then
        | when $role{X Y+1 Z-4}<>Role: E<=[0 0 0 1]
@@ -80,10 +80,10 @@ m_any_stairs Me X Y Z Role =
   else if E><[0 0 1 1] then
      if $role{X-1 Y Z+4}><Role then E<=[0 0 0 1]
      else if $role{X+1 Y Z-4}><Role and not $filled{X+1 Y Z} then E<=[0 0 0 1]
-     else if $role{X Y-1 Z-4}><Role and not $filled{X-1 Y Z} then E<=[0 1 1 1]
-     else if $role{X Y+1 Z+4}><Role then E<=[0 1 1 1]
-     else if $filled{X-1 Y Z+4} and not $filled{X Y+1 Z+4} then E<=[0 1 1 1]
-     else if not $filled{X+1 Y Z-4} then E<=[0 1 1 1]
+     else if $role{X Y-1 Z-4}><Role and not $filled{X-1 Y Z} then E<=[0 0 1 0]
+     else if $role{X Y+1 Z+4}><Role then E<=[0 0 1 0]
+     else if $filled{X-1 Y Z+4} and not $filled{X Y+1 Z+4} then E<=[0 0 1 0]
+     else if not $filled{X+1 Y Z-4} and $filled{X Y-1 Z-4} then E<=[0 0 1 0]
      else
   else if E><[1 0 0 0] then
      if $role{X-1 Y Z-4}><Role then E<=[0 1 0 0]
