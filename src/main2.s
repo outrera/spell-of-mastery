495aee125a033423a25b18dc5d5ee9ac8fb64aa1
saniv <snv985@gmail.com>
2016-08-05 17:45:32 +0300

    update

--- src/main2.s
+++ src/main2.s
@@ -48,7 +48,7 @@ type main{Data}
                "\n\n\nTHANKS TO:\n"
                "  Matthew Ostil\n"
               ]
-//| sprite_dump Me "/Users/nikita/Downloads/1" staticFrame/128 sheet/1 unit_devil
+//| sprite_dump Me "/Users/nikita/Downloads/0" staticFrame/100 sheet/1 unit_imp
 | init_unit_flags Me
 | world Me
 
