90cdc26269c9071eb90245a1c27f9c9b1f2ba4f6
Nikita Sadkov <snv985@gmail.com>
2017-10-21 01:37:44 +0300

    finished front view of the new goblin head

--- src/main2.s
+++ src/main2.s
@@ -55,7 +55,7 @@ type main{Data}
                "\n\n\nTHANKS TO:\n"
                "  Matthew Ostil\n"
               ]
-//| sprite_dump Me "/Users/nikita/Downloads/0" staticFrame/100 sheet/1 unit_vampire
+//| sprite_dump Me "work/sd" staticFrame/160 sheet/0 unit_goblin
 | init_unit_flags Me
 | world Me
 
