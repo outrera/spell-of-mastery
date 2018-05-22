da24564db0b4248c7e2c170ba69f6a48a52c1d6d
Nikita Sadkov <snv985@gmail.com>
2018-05-22 16:03:09 +0300

    highlight magic nodes on minimap

--- src/ui.s
+++ src/ui.s
@@ -138,7 +138,7 @@ ui.create_dialog_tabs StartTab =
           world($create_world_dlg)
           enter_site($create_enter_site_dlg)
 
-StartInEditor = 1
+StartInEditor = 0
 ui.init =
 | $mapsFolder <= "[$data][$mapsFolder]"
 | $savesFolder <= "[$data][$savesFolder]"
--- src/ui_widgets.s
+++ src/ui_widgets.s
@@ -180,6 +180,10 @@ minimap.draw G PX PY =
 | V = $main.ui.view
 | CX,CY,CZ = V.center
 | St = V.site
+| for U $main.site.active: when U.ai><node:
+  | VX = U.xyz.0*$w/St.w
+  | VY = U.xyz.1*$h/St.h
+  | G.circle{#00FF00 0 [PX+VX+8 PY+VY+16] 8}
 | VX = CX*$w/St.w
 | VY = CY*$h/St.h
 | G.circle{#FFFFFF 0 [PX+VX+4 PY+VY+4] 16}
