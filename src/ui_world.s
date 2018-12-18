1b1b916d4d36e8dfa06ab7737b4c0b360362ca13
Nikita Sadkov <snv985@gmail.com>
2018-12-18 15:09:26 +0200

    follow up

--- src/ui_world.s
+++ src/ui_world.s
@@ -44,7 +44,6 @@ ui.generate W H Blueprint =
 ui.place_ai_player =
 | Gold = $world.site_gold
 | DF = $cfg.world."df_[$enterSiteDst.type]"^~{1.0}
-| say DF
 | Gold <= (Gold.float*DF).int
 | Budget = max 400 (Gold*3+3)/4
 | SpellBudget = max 100 (Gold+3)/4
